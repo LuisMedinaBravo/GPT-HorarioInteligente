6946973beea8c104bb0ab28ccc333f28999cb705
Victor Sepulveda <victorsepulveda1994@gmail.com>
2023-05-22 17:31:34 -0400

    Avance de tabla para ver cursos de sala

--- public/js/leerSalas.js
+++ public/js/leerSalas.js
@@ -66,46 +66,92 @@ if(horarios.length==0){
                            
    `;
 }else{
-var z = document.getElementById('mostrar');
-var nombre_sala;
-var asignatura;
-var carrera;
-var profesor;
-var dia;
-var hora_inicio;
-var hora_fin;
-z.innerHTML= "<h1>La info de esta sala es:<h1/>";
+    var z = document.getElementById('mostrar');
+    var nombre_sala;
+    var asignatura;
+    var carrera;
+    var profesor;
+    var dia;
+    var hora_inicio;
+    var hora_fin;
+    z.innerHTML = `
+        <div class="filter">
+            <label for="building">Edificio:</label>
+            <select id="building">
+            <option value="">Todos</option>
+            <option value="edificio1">Edificio 1</option>
+            <option value="edificio2">Edificio 2</option>
+            <!-- Agrega más opciones de edificios aquí -->
+            </select>
+        </div>
 
-for (let index = 0; index < horarios.length; index++) {
-           
-                nombre_sala = horarios[index]['sala'];
-                asignatura = horarios[index]['asignatura'];
-                profesor = horarios[index]['profesor'];
-                dia = horarios[index]['dia'];
-                hora_inicio = horarios[index]['hora_inicio'];
-                hora_fin = horarios[index]['hora_fin'];
-                carrera = horarios[index]['carrera']
-                if (nombre_sala == "") {
-                    nombre_sala = "-";
-                }
-                //AlertaBien();
-                //alert("Sala encontrada, su información es: "+ users[n]['nombre']+ " "+ users[n]['piso']);
-                
-                z.innerHTML += `          
-                    <h2>Nombre de la sala: ${nombre_sala}</h2>
-                    <h2>Asignatura: ${asignatura}</h2>
-                    <h2>Carrera: ${carrera}</h2>
-                    <h2>Profesor: ${profesor}</h2>
-                    <h2>Dia: ${dia}</h2>
-                    <h2>Hora inicio: ${hora_inicio}</h2>
-                    <h2>Hora fin: ${hora_fin}</h2>
-                
-                `;
-              
-                    
-             
-             
-}
+        <div class="filter">
+            <label for="day">Día:</label>
+            <select id="day">
+            <option value="">Todos</option>
+            <option value="lunes">Lunes</option>
+            <option value="martes">Martes</option>
+            <!-- Agrega más opciones de días aquí -->
+            </select>
+        </div>
+
+        <div class="filter">
+            <label for="room">Sala:</label>
+            <select id="room">
+            <option value="">Todas</option>
+            <option value="sala1">Sala 1</option>
+            <option value="sala2">Sala 2</option>
+            <!-- Agrega más opciones de salas aquí -->
+            </select>
+        </div>
+
+        <table id="course-table">
+        <thead>
+        <tr>
+            <th>Curso</th>
+            <th>Edificio</th>
+            <th>Día</th>
+            <th>Sala</th>
+            <th>x</th>
+            <th>y</th>
+            <th>z</th>
+        </tr>
+        </thead>
+
+        <tbody>
+    `;
+
+    for (let index = 0; index < horarios.length; index++) {
+            
+        nombre_sala = horarios[index]['sala'];
+        asignatura = horarios[index]['asignatura'];
+        profesor = horarios[index]['profesor'];
+        dia = horarios[index]['dia'];
+        hora_inicio = horarios[index]['hora_inicio'];
+        hora_fin = horarios[index]['hora_fin'];
+        carrera = horarios[index]['carrera']
+        if (nombre_sala == "") {
+            nombre_sala = "-";
+        }
+        //AlertaBien();
+        //alert("Sala encontrada, su información es: "+ users[n]['nombre']+ " "+ users[n]['piso']);
+        
+        z.innerHTML += `
+            <tr>
+                <td>${nombre_sala}</td>
+                <td>${asignatura}</td>
+                <td>${carrera}</td>
+                <td>${dia}</td>
+                <td>${profesor}</td>
+                <td>${hora_inicio}</td>
+                <td>${hora_fin}</td>
+            </tr>
+        `;
+    }
+    z.innerHTML += `
+        </tbody>
+        </table>
+    `
 }
 //get all data
 // getDocs(collection(db, "horario")).then(docSnap => {
