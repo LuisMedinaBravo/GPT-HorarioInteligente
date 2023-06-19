a4d4732ed0d1754d34d74d0c483cc6c64486d1aa
LuisMedinaBravo <luis.medina.bravo@hotmail.com>
2023-06-18 22:01:10 -0400

    Busqueda sacada

--- public/js/leerSalasEstudiante.js
+++ public/js/leerSalasEstudiante.js
@@ -26,7 +26,8 @@ $(document).ready(async function () {
     });
 
     var table = $('#mydatatable').DataTable({
-        "dom": 'B<"float-left"i><"float-right"f>t<"float-left"l><"float-right"p><"clearfix">',
+        searching: true,
+        "dom": 'B<"float-left"i><"float-right"t<"float-left"l><"float-right"p><"clearfix">',
         "responsive": false,
         "language": {
             "url": "https://cdn.datatables.net/plug-ins/1.10.19/i18n/Spanish.json"
